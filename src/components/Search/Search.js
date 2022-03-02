7f598d9059abf758792e8258624e6204855e813c
MahirAshhab <mahir.ashhab77@gmail.com>
2022-03-02 16:42:11 +0600

    Javascript Implementation

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import Weather from '../Weather/Weather';
 import './Seacrh.css';
 
 const Search = () => {
+
+    const api = {
+        key: "d69ea9d6f4788c9545ec06595f50710d",
+        base: "https://api.openweathermap.org/data/2.5/"
+    }
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&APPID=d69ea9d6f4788c9545ec06595f50710d`)
+            .then(res => res.json())
+            .then(data => setWeather(data));
+    }, [])
+
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(data => {
+                    setWeather(data);
+                    setQuery('');
+                    console.log(data);
+                })
+        }
+    }
+
     return (
         <div>
             <div className="search-box">
@@ -9,9 +37,12 @@ const Search = () => {
                     type="text"
                     className="search-bar"
                     placeholder="Search"
+                    onChange={e => setQuery(e.target.value)}
+                    value={query}
+                    onKeyPress={search}
                 />
             </div>
-            <Weather></Weather>
+            <Weather weather={weather}></Weather>
         </div>
     );
 };
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,38 +1,88 @@
 import React from 'react';
 import './Weather.css';
 import cloud from '../../assets/cloudy.png';
+import rain from '../../assets/rainy.png';
+import sun from '../../assets/sunny.png';
+import thunder from '../../assets/thunder.png';
+import wind from '../../assets/windy.png';
+import snow from '../../assets/snowy.png';
 
-const Weather = () => {
+const Weather = (props) => {
+
+    const { weather } = props;
+
+    const dateBuilder = (props) => {
+        let months = ["January", "February", "March", "April",
+            "May", "June", "July", "August",
+            "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday"];
+
+        let day = days[props.getDay()];
+        let date = props.getDate();
+        let month = months[props.getMonth()];
+        let year = props.getFullYear();
+
+        return `${day}, ${month} ${date}, ${year}`;
+    }
+
+    const imageLoader = () => {
+        const i = weather.weather.length - 1;
+        if (parseInt(weather.weather[i].id) < 300) {
+            return thunder;
+        }
+
+        if (parseInt(weather.weather[i].id) > 299 && parseInt(weather.weather[i].id) < 600) {
+            return rain;
+        }
+
+        if (parseInt(weather.weather[i].id) > 599 && parseInt(weather.weather[i].id) < 700) {
+            return snow;
+        }
+
+        if (parseInt(weather.weather[i].id) > 699 && parseInt(weather.weather[i].id) < 800) {
+            return wind;
+        }
+
+        if (parseInt(weather.weather[i].id) === 800) {
+            return sun;
+        }
+
+        if (parseInt(weather.weather[i].id) > 800) {
+            return cloud;
+        }
+    }
+
     return (
         <div className="container">
             <div className="location-box">
                 <div className="location">
                     <h1>
-                        Dhaka
+                        {(typeof weather.main != "undefined") ? (weather.name) : ('')}
                         <span>, </span>
-                        BD
+                        {(typeof weather.main != "undefined") ? (weather.sys.country) : ('')}
                     </h1>
                 </div>
                 <div className="date">
                     <p>
-                        Tuesday, 1 March 2022
+                        {dateBuilder(new Date())}
                     </p>
                 </div>
             </div>
 
             <div className="weather-box">
                 <div className="weather-icon">
-                    <img src={cloud} alt="" />
+                    <img src={(typeof weather.main != "undefined") ? (imageLoader()) : ('')} alt="" />
                 </div>
                 <div className="temp">
                     <h1>
-                        19
+                        {(typeof weather.main != "undefined") ? (weather.main.temp) : ('')}
                         <span>° C</span>
                     </h1>
                 </div>
                 <div className="weather">
                     <p>
-                        Cloudy
+                        {(typeof weather.main != "undefined") ? (weather.weather[weather.weather.length - 1].main) : ('')}
                     </p>
                 </div>
             </div>
